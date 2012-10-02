35522bb8daa5c0d27a977989d93fabadf5c5b02a
Michael Rosenfeld <mdr224@cornell.edu>
2012-10-02 10:48:07 -0400

    Incorrectly Implemented Translation - Started

--- ppa1_student/src/cs4621/ppa1/manip/TranslateManip.java
+++ ppa1_student/src/cs4621/ppa1/manip/TranslateManip.java
@@ -1,11 +1,15 @@
 package cs4621.ppa1.manip;
 
+import java.util.Stack;
+
 import javax.media.opengl.GL;
 import javax.media.opengl.GL2;
+import javax.vecmath.Matrix4f;
 import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 
 import cs4621.ppa1.util.Util;
+import cs4621.ppa1.scene.TransformationNode;
 
 public class TranslateManip extends Manip
 {
@@ -13,6 +17,140 @@ public class TranslateManip extends Manip
 	public void dragged(Vector2f mousePosition, Vector2f mouseDelta)
 	{
 		// TODO: (Problem 1) Implement this manipulator.
+		Vector2f mouseAfter = new Vector2f(mousePosition.x + mouseDelta.x,
+										   mousePosition.y + mouseDelta.y);
+		
+		if(axisMode == PICK_CENTER){
+			Vector3f origin = new Vector3f(0, 0, 0);
+			
+			TransformationNode current = transformationNode.getLowestTransformationNodeAncestor();
+			while(current != null){
+				current.transform(origin, origin);
+				current = current.getLowestTransformationNodeAncestor();
+			}
+			
+			Vector3f p1 = camera.NDCToWorldAt(mousePosition, origin),
+					 p2 = camera.NDCToWorldAt(   mouseAfter, origin),
+					 p = new Vector3f(p2.x - p1.x, p2.y - p1.y, p2.z - p1.z);
+			
+			transformationNode.translation.x += p.x;
+			transformationNode.translation.y += p.y;
+			transformationNode.translation.z += p.z;
+		} else {
+			Vector3f p_a0 = new Vector3f(0, 0, 0),  // Before: Eye
+					 p_a1 = (Vector3f)p_a0.clone(), // Before: Mouse
+					 p_b0 = (Vector3f)p_a0.clone(), //  After: Eye
+					 p_b1 = (Vector3f)p_a0.clone(), //  After: Mouse
+					 ax_0 = (Vector3f)p_a0.clone(), // Axis0
+					 ax_1 = (Vector3f)p_a0.clone(); // Axis1
+			
+			if(axisMode == PICK_X) ax_1.x = 1;
+			else if(axisMode == PICK_Y) ax_1.y = 1;
+			else if(axisMode == PICK_Z) ax_1.z = 1;
+					 
+			
+			camera.getLineThroughNDC(mousePosition, p_a0, p_a1);
+			camera.getLineThroughNDC(mouseAfter   , p_b0, p_b1);
+			
+			TransformationNode current = transformationNode.getLowestTransformationNodeAncestor();
+			//Stack<Matrix4f> matrices = new Stack<Matrix4f>();
+			
+			while(current != null){
+				/*Matrix4f scale = new Matrix4f(),
+						 rotX  = new Matrix4f(),
+						 rotY  = new Matrix4f(),
+						 rotZ  = new Matrix4f(),
+						 trans = new Matrix4f();
+				
+				scale.m00 = current.scaling.x;
+				scale.m11 = current.scaling.y;
+				scale.m22 = current.scaling.z;
+				scale.m33 = 1;
+				
+				matrices.push(scale);
+				
+				Vector3f rotation = current.rotation;
+				
+				rotX.m00 = 1;
+				rotY.m11 = 1;
+				rotZ.m22 = 1;
+				
+				double aX = toRadians(rotation.x);
+				rotX.m11 = (float)Math.cos(aX);
+				rotX.m12 = -(float)Math.sin(aX);
+				rotX.m22 = (float)Math.cos(aX);
+				rotX.m21 = (float)Math.sin(aX);
+				
+				double aY = toRadians(rotation.y);
+				rotY.m00 = (float)Math.cos(aY);
+				rotY.m02 = (float)Math.sin(aY);
+				rotY.m22 = (float)Math.cos(aY);
+				rotY.m20 = -(float)Math.sin(aY);
+				
+				double aZ = toRadians(rotation.z);
+				rotZ.m00 = (float)Math.cos(aZ);
+				rotZ.m01 = -(float)Math.sin(aZ);
+				rotZ.m11 = (float)Math.cos(aZ);
+				rotZ.m10 = (float)Math.sin(aZ);
+				
+				matrices.push(rotX);
+				matrices.push(rotY);
+				matrices.push(rotZ);
+				
+				trans.m03 = current.translation.x;
+				trans.m13 = current.translation.y;
+				trans.m23 = current.translation.z;
+				trans.m33 = 1;
+				
+				matrices.push(trans);*/
+				
+				/*current.transform(p_a0, p_a0);
+				current.transform(p_a1, p_a1);
+				current.transform(p_b0, p_b0);
+				current.transform(p_b1, p_b1);*/
+				//current.transform(ax_0, ax_0);
+				//current.transform(ax_1, ax_1);
+				 //should i be multiplying by inverses?****
+				current = current.getLowestTransformationNodeAncestor();
+			}
+			
+			/*while(matrices.size() > 0){
+				Matrix4f m = matrices.pop();
+				m.invert();
+				m.transform(p_a0);
+				m.transform(p_a1);
+				m.transform(p_b0);
+				m.transform(p_b1);
+			}*/
+			
+			float t0, t1;
+			t0 = Util.lineNearLine(ax_0, ax_1, p_a0, p_a1);
+			t1 = Util.lineNearLine(ax_0, ax_1, p_b0, p_b1);
+			switch(axisMode){
+			case PICK_X:
+				//t0 = Util.lineNearLine(new Vector3f(0, 0, 0), new Vector3f(1, 0, 0), p_a0, p_a1);
+				//t1 = Util.lineNearLine(new Vector3f(0, 0, 0), new Vector3f(1, 0, 0), p_b0, p_b1);
+				transformationNode.translation.x += t1 - t0;
+				
+				break;
+				
+			case PICK_Y:
+				//t0 = Util.lineNearLine(new Vector3f(0, 0, 0), new Vector3f(0, 1, 0), p_a0, p_a1);
+				//t1 = Util.lineNearLine(new Vector3f(0, 0, 0), new Vector3f(0, 1, 0), p_b0, p_b1);
+				transformationNode.translation.y += t1 - t0;
+				break;
+				
+			case PICK_Z:
+				//t0 = Util.lineNearLine(new Vector3f(0, 0, 0), new Vector3f(0, 0, 1), p_a0, p_a1);
+				//t1 = Util.lineNearLine(new Vector3f(0, 0, 0), new Vector3f(0, 0, 1), p_b0, p_b1);
+				transformationNode.translation.z += t1 - t0;
+				break;
+			}
+		}
+	}
+	
+	private double toRadians (float a) {
+		return ((float)a * Math.PI) / 180;
 	}
 
 	private Vector3f xAxis = new Vector3f();
